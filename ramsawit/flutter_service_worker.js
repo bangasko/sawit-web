0efb71be63abafede3e62fb57a17f5b7742beead
bangasko <bangasko@users.noreply.github.com>
2026-09-07 05:15:11 +0000

    deploy: bangasko/Flutter-ku@6b4860ff801f818526f9b65c750a65f35a31b833

--- ramsawit/flutter_service_worker.js
+++ ramsawit/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "42b76e4907efa6677fe69344aa9098f0",
 "version.json": "3e238221f11cd3a219458b45bc90858f",
-"main.dart.js": "141f77b8cc7f1b59c9edb05c0387e64a",
-"flutter_bootstrap.js": "bdd2b1080c18ed115a41dae49f470c7b",
+"main.dart.js": "976bbefd57058fdde56cc10bc1b188e2",
+"flutter_bootstrap.js": "d8fbb3b36d1db207be66a667d59d2edb",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
