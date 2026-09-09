37ff4924a28b859e28066a298d0006943c61c639
bangasko <bangasko@users.noreply.github.com>
2026-09-09 09:47:53 +0000

    deploy: bangasko/Flutter-ku@2a7b9f1fd1a6d3bb3c31cd34e9fbfba3a4796c42

--- tokesawit/flutter_bootstrap.js
+++ tokesawit/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2698566802"
+    serviceWorkerVersion: "3163103799"
   }
 });
--- tokesawit/flutter_service_worker.js
+++ tokesawit/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "42b76e4907efa6677fe69344aa9098f0",
 "version.json": "3e238221f11cd3a219458b45bc90858f",
-"main.dart.js": "0485735f12cca3be7d0c8278079f82a9",
-"flutter_bootstrap.js": "542226e0a19a918f2ac85e553a0522c0",
+"main.dart.js": "051e77a317788f39ac7429f7643d71f8",
+"flutter_bootstrap.js": "26a2521d4c7d8ddc157d174c6cead28c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
