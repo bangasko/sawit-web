24870ee69b085f1a4e66e50dc586846b2de313de
bangasko <bangasko@users.noreply.github.com>
2026-09-23 07:55:02 +0000

    deploy: bangasko/Flutter-ku@c2e12d03445be03a911c4283e7a6d91c7c2ed736

--- ramsawit/flutter_bootstrap.js
+++ ramsawit/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2894096259"
+    serviceWorkerVersion: "3744738051"
   }
 });
